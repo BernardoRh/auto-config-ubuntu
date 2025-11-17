1b4bb666f4c7c600336c0937deecd664632cae9a
Bernardorh <bernardo.horbach@gmail.com>
2025-11-17 11:15:51 -0300

    First test to update the computers

--- STARTUP_SCRIPT.sh
+++ STARTUP_SCRIPT.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 #Here will be all the scripts the machine will run every time it starts up
-sudo echo "Testando Testando Testando Testando Testando" > /home/aluno/.hidden/log
+sudo apt install klavaro
+sudo apt purge waydroid 
