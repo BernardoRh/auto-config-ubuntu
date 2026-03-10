cdb437a319129597f4580edebd2fe33395fb3748
Bernardo Rodrigo Horbach <56304604+BernardoRh@users.noreply.github.com>
2026-03-10 09:48:22 -0300

    Update STARTUP_SCRIPT.sh

--- STARTUP_SCRIPT.sh
+++ STARTUP_SCRIPT.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 #Here will be all the scripts the machine will run every time it starts up
 sudo CTparental -on
+echo "rachacuca.com.br
+wordwall.net
+www.coquinhos.com
+web.whatsapp.com
+escolagames.com.br" > /etc/CTparental/dip-rehabiliter.conf;
