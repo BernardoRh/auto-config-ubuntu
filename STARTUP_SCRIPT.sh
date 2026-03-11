f933ad475f08b41825d9da31ed50405ace71ba3d
Bernardo Rodrigo Horbach <56304604+BernardoRh@users.noreply.github.com>
2026-03-11 09:43:27 -0300

    Update STARTUP_SCRIPT.sh

--- STARTUP_SCRIPT.sh
+++ STARTUP_SCRIPT.sh
@@ -5,4 +5,4 @@ echo "rachacuca.com.br
 wordwall.net
 www.coquinhos.com
 web.whatsapp.com
-www.escolagames.com.br" > /etc/CTparental/dip-rehabiliter.conf;
+escolagames.com.br" > /etc/CTparental/dip-rehabiliter.conf;
