bf72f8742b09c23971c8474cd9e06babadd8afe9
Bernardo Rodrigo Horbach <56304604+BernardoRh@users.noreply.github.com>
2026-03-18 08:15:15 -0300

    Update STARTUP_SCRIPT.sh

--- STARTUP_SCRIPT.sh
+++ STARTUP_SCRIPT.sh
@@ -7,7 +7,64 @@ coquinhos.com
 whatsapp.com
 escolagames.com.br" > /etc/CTparental/dip-rehabiliter.conf;
 
-
+echo "youtube.com
 chatgpt.com
 friv.com
-poki.com
+poki.com" > /etc/CTparental/dip-blackliste.conf;
+
+echo "adult
+adultsearchengine
+agressif
+associations_religieuses
+astrology
+audio-video
+bitcoin
+blog
+celebrity
+cryptojacking
+ct_doh_dot_doq
+ctparental
+dangerous_material
+dating
+ddos
+dialer
+doh
+drogue
+dynamic-dns
+exceptions_liste_bu
+fakenews
+filehosting
+financial
+forums
+gambling
+hacking
+lingerie
+malware
+manga
+marketingware
+mixed_adult
+mobile-phone
+pgl_yoyo_org
+phishing
+press
+publicite
+radio
+reaffected
+redirector
+residential-proxies
+sect
+shopping
+shortener
+social_networks
+special
+sports
+stalkerware
+strict_redirector
+strong_redirector
+tor_nodes
+tricheur
+tricheur_pix
+ultrasurf_nodes
+vpn
+warez
+webhosting" > /etc/CTparental/categories-enabled.conf;
