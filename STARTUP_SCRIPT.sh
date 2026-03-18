550468d1a6f063949eb9711f77031a48917a0059
Bernardo Rodrigo Horbach <56304604+BernardoRh@users.noreply.github.com>
2026-03-18 08:21:54 -0300

    Update STARTUP_SCRIPT.sh

--- STARTUP_SCRIPT.sh
+++ STARTUP_SCRIPT.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 #Here will be all the scripts the machine will run every time it starts up
-sudo CTparental -on
+sudo CTparental -off
 echo "rachacuca.com.br
 wordwall.net
 coquinhos.com
 whatsapp.com
 escolagames.com.br" > /etc/CTparental/dip-rehabiliter.conf;
-
 echo "youtube.com
 chatgpt.com
 friv.com
 poki.com" > /etc/CTparental/dip-blackliste.conf;
-
 echo "adult
 adultsearchengine
 agressif
@@ -68,3 +66,4 @@ ultrasurf_nodes
 vpn
 warez
 webhosting" > /etc/CTparental/categories-enabled.conf;
+sudo CTparental -on
